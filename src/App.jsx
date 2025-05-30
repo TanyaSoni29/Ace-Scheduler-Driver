445ee7b33a56508e040c52dad281d6f636204e10
TanyaSoni2912 <tanu139sona@gmail.com>
2025-05-30 13:01:14 +0530

    token

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 /** @format */
 
+import { useParams } from 'react-router-dom';
 import AceScheduler from './page/Scheduler';
 
 function App() {
+	const { token } = useParams();
+	if (token) localStorage.setItem('token', token);
 	return (
 		<>
 			<AceScheduler />
